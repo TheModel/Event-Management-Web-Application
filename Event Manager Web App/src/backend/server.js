6076c80da731e9a4f43f2496dbb7e21778ac8b78
GWorld1 <thunder5613@gmail.com>
2024-05-14 10:17:13 +0100

    Setup Web Server

---
+++ Event Manager Web App/src/backend/server.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import express from 'express'
+import cors from 'cors'
+import bodyParser from "body-parser";
+import { router as EventRoutes } from "./routes/event.routes";
+import { mongodb_connection } from "../api/mongodb";
+
+
+//Create Web Server Insatnce using express.js
+const app = express();
+const PORT = 3000;
+
+//Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/events',EventRoutes)
+//Connect to MongoDB
+mongoose.connect(mongodb_connection,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(()=>{
+    console.log("Connected to Database");
+    app.listen(PORT,()=>{
+        console.log(`Server listening on port ${PORT}`)
+    })
+})
+.catch(()=>{
+    console.log('Connection to database failed')
+})
