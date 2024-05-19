31277199c21b9f392453637647f993a74ba5c0e3
TheModel <152254955+TheModel@users.noreply.github.com>
2024-05-18 23:09:37 -0700

    Update NavBar.jsx

--- Event Manager Web App/src/Components/NavigationBar/NavBar.jsx
+++ Event Manager Web App/src/Components/NavigationBar/NavBar.jsx
@@ -1,7 +1,14 @@
+import './NavBar.css'
 const NavBar = () => {
   return (
     <div className="navbar">
-
+        <img src="" alt="Logo of Eventify"></img>
+        <ul className="navbar-menu">
+            <li>Home</li>
+            <li>About</li>
+            <li>Events</li>
+            <li>Contact Us</li>
+        </ul>
     </div>
   )
 }
