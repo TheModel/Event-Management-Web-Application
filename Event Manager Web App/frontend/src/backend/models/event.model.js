179acc5d52ffe283c10d034a9086be738352f0f1
Maestro-jr <146443394+Maestro-jr@users.noreply.github.com>
2024-05-20 22:32:50 +0100

    REInputtinmg backend

---
+++ Event Manager Web App/frontend/src/backend/models/event.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const eventSchema = new  mongoose.Schema(
+    {
+        title:{ type: String , required: true},
+        date:{type: Date , required:true},
+        reminder:{type: Boolean , default:false}
+    }
+)
+
+export const Event = mongoose.model('Event',eventSchema);
+
