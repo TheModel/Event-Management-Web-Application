37f26ac449aa60d7c8bd4af777b969939ec6e281
TheModel <152254955+TheModel@users.noreply.github.com>
2024-05-19 23:44:49 -0700

    Update NavBar.jsx with icon logo

--- Event Manager Web App/src/Components/NavigationBar/NavBar.jsx
+++ Event Manager Web App/src/Components/NavigationBar/NavBar.jsx
@@ -2,7 +2,7 @@ import './NavBar.css'
 const NavBar = () => {
   return (
     <div className="navbar">
-        <img src="" alt="Logo of Eventify"></img>
+        <img src="./src/assets/eventLogo.png" alt="Logo of Eventify"></img>
         <ul className="navbar-menu">
             <li>Home</li>
             <li>About</li>
