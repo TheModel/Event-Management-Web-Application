f8c5ae55c7720bb17ff15d8a304ef8ef77c777cc
TheModel <152254955+TheModel@users.noreply.github.com>
2024-05-20 02:28:02 -0700

    Completion of Navigation Bar development
    Here, I completed its development by creating the Home, About, Contact Us, and many other important details of the website.

---
+++ Event Manager Web App/src/Pages/AddToEventList/AddToEventList.jsx
@@ -0,0 +1,12 @@
+// This file has to deal with a User adding an Event to the list of events he wants to attend.
+// It is related to the navigation bar, where there is event
+
+import React from 'react'
+import './AddToEventList.css'
+const AddToEventList = () => {
+  return (
+    <div>AddToEventList</div>
+  )
+}
+
+export default AddToEventList
