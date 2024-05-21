82ca1330369298d89ba3e4fe5c782cc366d8c396
TheModel <152254955+TheModel@users.noreply.github.com>
2024-05-21 15:31:02 -0700

    Main Changes On Admin Add

---
+++ Event Manager Web App/src/AdminMain.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Admin from './Admin'
+import './AdminIndex.css'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Admin />
+  </BrowserRouter>
+    
+  
+)
