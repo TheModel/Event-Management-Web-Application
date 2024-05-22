0def29b1d7f2b6f0d536e5342c7f8c09d4255d06
Kangajrdeo <151434188+Kangajrdeo@users.noreply.github.com>
2024-05-22 12:34:55 +0100

    Update AppDownload.jsx

--- Event Manager Web App/src/components/AppDownload/AppDownload.jsx
+++ Event Manager Web App/src/components/AppDownload/AppDownload.jsx
@@ -10,7 +10,9 @@ const AppDownload = () => {
                 <a href="https://play.google.com" target="_blank" rel="noopener noreferrer">
                   <img src={assets.play_store} alt="Play Store" />
                 </a>
-                <img src={assets.app_store} alt="" />
+                <a href="https://www.apple.com/app-store/" target="_blank" rel="noopener noreferrer">
+                  <img src={assets.app_store} alt="App Store" />
+                </a>
             </div>
         </div>
     )
