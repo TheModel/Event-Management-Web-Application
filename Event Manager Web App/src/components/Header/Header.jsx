39960d89c07b0c86418f9effc86816a05da582e7
TheModel <152254955+TheModel@users.noreply.github.com>
2024-05-21 21:21:52 -0700

    Deleting Header.js

--- Event Manager Web App/src/components/Header/Header.jsx
+++ Event Manager Web App/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     const navigate = useNavigate();
 
     const openNewPage = () => {
-        // Open the specified URL in a new tab or window
+        // Navigate to the /admin route
         navigate('/admin');
     };
 
@@ -13,11 +13,13 @@ const Header = () => {
         <div className='header'>
             <div className='header-contents'>
                 <h2>Your Events Start Here!</h2>
-                <p>Events are exquisite orchestrations of time, space, and human connection, where moments unfold like delicate petals in a blossoming garden of experiences. Each gathering is a tapestry woven with threads of anticipation, excitement, and celebration, creating a symphony of memories that linger in the hearts of attendees. </p>
-             
-                <button className={'btn'} onClick={openNewPage}>Add Event</button>
-        
-                
+                <p>
+                    Events are exquisite orchestrations of time, space, and human connection, where moments unfold 
+                    like delicate petals in a blossoming garden of experiences. Each gathering is a tapestry woven 
+                    with threads of anticipation, excitement, and celebration, creating a symphony of memories that 
+                    linger in the hearts of attendees.
+                </p>
+                <button className='btn' onClick={openNewPage}>Add Event</button>
             </div>
         </div>
     );
