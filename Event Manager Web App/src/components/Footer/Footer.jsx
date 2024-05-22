22269908a775afbb6cbcf04f9d76c020a2dd7c28
TheModel <152254955+TheModel@users.noreply.github.com>
2024-05-22 11:54:12 -0700

    Changing file Structure

---
+++ Event Manager Web App/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>Empowering Memorable Experiences – At Eventify, we are dedicated to helping you create unforgettable events with ease and efficiency. From planning to execution, we provide innovative tools and exceptional service to ensure every detail is perfect.</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+237-652-423-576</li>
+                <li>contact@eventify.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © Eventify.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ Event Manager Web App/src/pages/Admin.jsx
@@ -0,0 +1,17 @@
+import Sidebar from '../components/Sidebar/Sidebar'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Admin = () => {
+  return ( 
+    <div className='app'>
+      <ToastContainer/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+      </div>
+    </div>
+  )
+}
+
+export default Admin
