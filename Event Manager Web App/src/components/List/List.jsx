ae2ccdbd9cf13c7d335a3d324beffcc78b7f1e9a
Fung-Nkoh-Manuel <153408741+Fung-Nkoh-Manuel@users.noreply.github.com>
2024-05-26 08:18:48 +0100

    make changes in main

--- Event Manager Web App/src/components/List/List.jsx
+++ Event Manager Web App/src/components/List/List.jsx
@@ -3,6 +3,21 @@ import './List.css'
 
 const List = () => {
 
+  return (
+    <div className='list add flex-col'>
+        <p className='my-paragraph'>My Added Events</p>
 
+        <div className='list-table'>
+          <div className="list-table-format title">
+            <b>Image</b>
+            <b>Name</b>
+            <b>Category</b>
+            <b>Action</b>
+          </div>
+        </div>
+    </div>
+    
+  )
+}
 
 export default List
