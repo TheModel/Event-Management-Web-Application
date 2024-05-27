56e20e2478ddc10969822089cfb81314b1be9455
FontchaSteve <153062747+FontchaSteve@users.noreply.github.com>
2024-05-27 19:32:45 +0100

    Update Footer.jsx

--- Event Manager Web App/src/components/Footer/Footer.jsx
+++ Event Manager Web App/src/components/Footer/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © Eventify.com - All Rights Reserved.</p>
+      <p className="footer-copyright">Copyrights 2024 © Eventify.com - All Rights Reserved.</p>
     </div>
   )
 }
